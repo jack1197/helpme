1587d402b59a34d25818191bba958082475b9a1d
Jack Wilkie <im-not-tiger@hotmail.com>
2016-08-16 20:05:54 +1200

    bug

--- backend/header.php
+++ backend/header.php
@@ -56,7 +56,7 @@
         </h1>
     </nav>
     <div class="container main">
-        <div class="col-sm-12 header">
+        <div class="col-sm-12 page-header">
             <?php
             if (isset($title)) 
             {
