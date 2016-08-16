b5b40e7ea455e469cff7e5b598a9ae03fb809523
jack1197 <im-not-tiger@hotmail.com>
2016-08-16 15:00:15 +1200

    trying to fix include

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-include '/backend/footer.php'; 
+include($_SERVER['DOCUMENT_ROOT'].'/backend/footer.php'); 
 ini_set("display_errors", 1);
 ?>
         <h1>Help me! Class Queuing</h1>
-<?php include '/backend/footer.php'; ?>
+<?php include ($_SERVER['DOCUMENT_ROOT'].'/backend/footer.php'); ?>
