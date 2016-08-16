025eba7c508ed226abca555a1a8146d1f634bf6e
jack1197 <im-not-tiger@hotmail.com>
2016-08-16 12:47:12 +1200

    Improved style, started php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html lang="en">
 
 <head>
-    <title>Bootstrap Theme Simply Me</title>
+    <title>Help Me! Class Queue</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/style/index.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
@@ -20,7 +21,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class=navbar-brand " href="# ">Help me! (DEV)</a>
+                <a class="navbar-brand" href="# ">Help me! (DEV)</a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
@@ -37,7 +38,8 @@
         </div>
         </h1>
     </nav>
-    <div class="container ">
+    <div class="container">
+        <h1>Help me! Class Queuing</h1>
     </div>
 
 </body>
