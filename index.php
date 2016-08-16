9c5bf7c9b32ff6283625bba84232193aa69908bf
jack1197 <im-not-tiger@hotmail.com>
2016-08-16 12:51:32 +1200

    fix

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php include 'backend/footer.php' ?>
+<?php include 'backend/footer.php'; ?>
         <h1>Help me! Class Queuing</h1>
-<?php include 'backend/footer.php' ?>
+<?php include 'backend/footer.php'; ?>
