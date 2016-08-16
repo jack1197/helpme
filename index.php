ac77fef37e0b0a56d1c87e44c542d02f8e99aea1
jack1197 <im-not-tiger@hotmail.com>
2016-08-16 14:53:32 +1200

    increased error reporting

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 include 'backend/footer.php'; 
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(-1);
 ?>
         <h1>Help me! Class Queuing</h1>
 <?php include 'backend/footer.php'; ?>
