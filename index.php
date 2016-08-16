f01bf8efeb5722eb1dcc7004280897f7f01dcde1
jack1197 <im-not-tiger@hotmail.com>
2016-08-16 15:07:28 +1200

    moar

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-include($_SERVER['DOCUMENT_ROOT'].'/backend/footer.php'); 
 ini_set("display_errors", 1);
+include($_SERVER['DOCUMENT_ROOT'].'/backend/head.php');
 ?>
         <h1>Help me! Class Queuing</h1>
-<?php include ($_SERVER['DOCUMENT_ROOT'].'/backend/footer.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/backend/footer.php'); ?>
