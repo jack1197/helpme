7cc0fc49a579f27e9ba084069241e21c97d80edb
jack1197 <im-not-tiger@hotmail.com>
2016-08-16 16:11:47 +1200

    actual dummy pages

---
+++ register.php
@@ -0,0 +1,7 @@
+<?php 
+$currpage = 'Register';
+ini_set("display_errors", 1);
+require($_SERVER['DOCUMENT_ROOT'].'/backend/header.php');
+?>
+
+<?php require($_SERVER['DOCUMENT_ROOT'].'/backend/footer.php'); ?>
