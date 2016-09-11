2f74c84d04ce2b7fd0bb281339be2d0283611c2e
jack1197 <im-not-tiger@hotmail.com>
2016-09-12 10:48:12 +1200

    compatibility mistake

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        'MSApplicationInsightsMiddleware',
     ];
 
     /**
@@ -30,6 +29,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            'MSApplicationInsightsMiddleware',
         ],
 
         'api' => [
