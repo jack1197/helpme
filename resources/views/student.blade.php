1b383f2fac4f72eb5a1f36d4f963ff4413fe1010
jack1197 <im-not-tiger@hotmail.com>
2016-09-11 14:21:47 +1200

    changed student page title

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Student page</div>
+                <div class="panel-heading"><h2>Student access</h2></div>
 
                 <div class="panel-body">
                     @if (Auth::guest() )
